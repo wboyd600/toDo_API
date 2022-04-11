4823f96cfd135e0a5e93c13e971dea78a03c6f74
Wesley <wesleyboyd@tinyhomeconsultingllc.com>
2022-04-11 10:15:11 -0400

    got rid of some warnings

--- Models/LoginResponse.Data.cs
+++ Models/LoginResponse.Data.cs
@@ -2,8 +2,8 @@ namespace toDo_API.Models
 {
     partial class LoginResponse {
         public class Data {
-            public string token {get; set;}
-            public Guid id {get; set;}
+            public string token { get; set; } = string.Empty;
+            public Guid id { get; set; }
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -5,16 +5,16 @@ namespace toDo_API.Models
     public class User: IEntity<Guid>
     {
         public Guid Id { get; set; }
-        public String Username { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Salt { get; set; } = string.Empty;
     }
-    
+
     public class CreateUser {
-        public Data data { get; set; }
+        public Data data { get; set; } = new Data();
         public class Data {
-            public String Username { get; set; }
-            public string Password { get; set; }
+            public string Username { get; set; } = string.Empty;
+            public string Password { get; set; } = string.Empty;
         }
     }
 
@@ -22,14 +22,14 @@ namespace toDo_API.Models
     {
         public UserData(
             Guid id,
-            String username
+            string username
         ) 
         {
             this.Id = id;
             this.Username = username;
         }
         public Guid Id { get; set; }
-        public String Username { get; set; }
+        public string Username { get; set; }
     }
 
     public class Message
